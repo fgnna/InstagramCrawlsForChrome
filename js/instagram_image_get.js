7ed342fb586783db84ce5916b6cb0dbcad030ef5
jie <fgnna@zuqiukong.com>
2016-09-06 14:11:11 +0800

    s

--- js/instagram_image_get.js
+++ js/instagram_image_get.js
@@ -1,29 +1,50 @@
 
-alert(window.location.href);
-//window.location.href="https://www.instagram.com/manchesterunited/"; 
-$(function(){
-	alert(1);
-        setTimeout("findImg()",3000);
+$(function()
+{
+	var src = window.location.href;
+	var patt1=/https:\/\/www.instagram.com\/\w*\/{1}$/;
+
+	if(patt1.test(src))
+	{
+		setTimeout("findImg()",6000);
+	}
+        	
 });
 
 var key1 = "https://www.instagram.com/p/";
 var key2 = false;
+var datasUrl = new Array();
+var count = 0;
 function findImg()
 {
-
-
-
 	$("a").each(function(i)
 	{
-		if(-1 != this.href.indexOf(key1)&& !key2)
+		if(-1 != this.href.indexOf(key1))
 		{
-			alert(this.href);
-			key2 = true;
-			$(this).children().eq(0).click();
+			datasUrl[count++] = this.href+"&__a=1";
 		}
-
-		
-
 	});
 
+	if(count == 12)
+	{
+		loadData();
+	}
+		
+}
+
+function loadData()
+{
+	$.getJSON(datasUrl[0], function(data)
+	{
+		alert(data.media.display_src);
+		var item ={};
+		item.url = data.media.display_src;
+		item.filename = "test.jpg";
+		
+		chrome.downloads.download(item,function(downloadId){
+
+				alert(downloadId);
+
+		});
+	});
 }
